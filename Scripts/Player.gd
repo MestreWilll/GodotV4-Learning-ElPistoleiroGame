6d8cbb5d8a00467e658bee2686c534e7727c7d2d
mestrewilll <willianichigo@hotmail.com>
2024-01-23 02:26:01 -0300

    KnockbackfixDistance

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
-
 const SPEED = 280.0
 const JUMP_VELOCITY = -530.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animation = $AnimatedSprite2D as AnimatedSprite2D
 @onready var remote_transform = $remote as RemoteTransform2D
+@onready var ray_left = $RayCast2D_Left as RayCast2D
+@onready var ray_right = $RayCast2D_Right as RayCast2D
 var knockback_vector = Vector2()
 var is_running = false
 var is_jumping = false
@@ -62,6 +63,20 @@ func _physics_process(delta):
 		# Amortece o knockback_vector para que o efeito diminua ao longo do tempo
 		knockback_vector = knockback_vector.move_toward(Vector2.ZERO, 1000 * delta)  # Ajuste a taxa de amortecimento conforme necessário
 
+	# Verifica se o raycast à esquerda detecta algo
+	if ray_left.is_enabled() and ray_left.is_colliding():
+		var collider = ray_left.get_collider()
+		print("Colisão detectada à esquerda com: ", collider.name)
+		# Lógica para quando algo é detectado à esquerda
+		handle_collision(collider, "left")
+
+	# Verifica se o raycast à direita detecta algo
+	if ray_right.is_enabled() and ray_right.is_colliding():
+		var collider = ray_right.get_collider()
+		print("Colisão detectada à direita com: ", collider.name)
+		# Lógica para quando algo é detectado à direita
+		handle_collision(collider, "right")
+
 	move_and_slide()
 
 func follow_camera(camera):
@@ -92,6 +107,11 @@ func _on_hurtbox_body_entered(body):
 		
 		print("Vc tomou dano")
 
-
 func _on_animated_sprite_2d_animation_finished():
 	pass # Replace with function body.
+
+func handle_collision(collider, direction):
+	if collider.is_in_group("enemies"):
+		# Aqui você pode adicionar a lógica específica para quando um inimigo é detectado
+		# por exemplo, aplicar knockback na direção oposta
+		print("Inimigo detectado à " + direction)
