895ff6f30e74b729defb483a0620fd7ee1d9a15a
mestrewilll <willianichigo@hotmail.com>
2024-01-31 19:45:45 -0300

    Estrutura_Atualizada: 4.2

--- Scripts/game.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-# This variable keeps track of the number of coins collected
-var coins_collected = 0
-
-# Function to increment the count of coins
-func add_coin():
-	coins_collected += 1
-	# Here you can emit a signal or call a function to update the UI
--- mundo_01.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-@onready var Player := $Player as CharacterBody2D
-@onready var camera := $camera as Camera2D
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	Player.follow_camera(camera)
