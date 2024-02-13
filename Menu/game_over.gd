a5d9e0968e6bdfde55f76d0ef9012f071ce64c27
mestrewilll <willianichigo@hotmail.com>
2024-02-13 07:08:50 -0300

    fix-CalculosBotões

--- Menu/game_over.gd
+++ Menu/game_over.gd
@@ -12,8 +12,12 @@ func _process(delta):
 
 
 func _on_restart_button_pressed():
-	get_tree().change_scene_to_file("res://Menu/title_screen.tscn")
+	get_tree().change_scene_to_file("res://.godot/exported/133200997/export-d5b13bfc9b0c6da6031422aa5ac56d9f-Mundo.scn")
 
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_volta_menu_pressed():
+	get_tree().change_scene_to_file("res://.godot/exported/133200997/export-3c0ae5175927600a5da40564799297aa-title_screen.scn")
