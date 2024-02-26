24db40589e0617868147dce172061f898dca189c
mestrewilll <willianichigo@hotmail.com>
2024-02-26 16:25:28 -0300

    fix-request-local

--- Menu/game_over.gd
+++ Menu/game_over.gd
@@ -1,18 +1,20 @@
 extends Control
 
-@onready var time_label = $time_label
-@onready var score_label = $score_label
-@onready var coins_label = $coins_label
+@onready var time_label = $Control_label_request/time_label
+@onready var score_label = $Control_label_request/score_label
+@onready var coins_label = $Control_label_request/coins_label
+@onready var name_label = $Control_label_request/name_label
+
 
 
 func _ready():
 
-	time_label.text = Game.player_death_time
+	time_label.text = Game.timer_counter
 	score_label.text = str("%06d" % Game.score)
 	coins_label.text = str("%04d" % Game.coins)
 	
 	print("Meu numero de Score: ", Game.score) 
-	print("Tempo de morte do jogador na tela de Game Over: ", Game.player_death_time) 
+	print("Tempo de morte do jogador na tela de Game Over: ", Game.timer_counter) 
 	print("Coins que foram pegos: ", Game.coins) 
 		
 func _on_restart_button_pressed():
--- Scripts Inimigos/game.gd
+++ Scripts Inimigos/game.gd
@@ -3,4 +3,6 @@ extends Node
 var coins := 0
 var score := 0
 var player_life := 3
-var player_death_time := ""  # Adiciona a variável para armazenar todos pontos
+var timer_counter := ""
+var minutes = 0
+var seconds = 0
