5a407f498dbe9f62404a3645c4b890e5dcd76cff
Weverton Lima <wevertonslima@gmail.com>
2024-02-27 22:03:35 -0300

    sampla json server

--- Send_Request/send_request.gd
+++ Send_Request/send_request.gd
@@ -5,7 +5,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$HTTPRequest.request_completed.connect(_on_request_completed)
-	$HTTPRequest.request("http://localhost:8080/")
+	$HTTPRequest.request("http://192.168.1.11:8080/")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_request_completed(result, response_code, headers, body):
