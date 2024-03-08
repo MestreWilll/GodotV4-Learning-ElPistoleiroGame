4f9956e7a8ef1a2ad42b9d97a97d20df0e5148b9
mestrewilll <willianichigo@hotmail.com>
2024-03-08 01:47:21 -0300

    enemy_graity mod = enemy_bullet_base

--- Scripts Inimigos/enemy_attack.gd
+++ Scripts Inimigos/enemy_attack.gd
@@ -7,6 +7,7 @@ var health_points := 3
 @onready var ground_detector = $ground_detector
 @onready var player_detector_left = $player_detector_left
 @onready var player_detector_right = $player_detector_right
+@onready var spawn_bullet = $spawn_bullet
 
 # Variável exportada para controlar o comportamento de gravidade
 @export var gravity_mode := 1
@@ -14,7 +15,7 @@ var health_points := 3
 # Constantes para as camadas de colisão
 const LAYER_MUNDO := 1
 const LAYER_PLATAFORMAS := 2
-
+const BULLET_ENEMY_SCENE = preload("res://Inimigos_cenario/bullet_enemy.tscn")
 # Variáveis para gravidade e velocidade vertical
 var gravity := 300
 var velocity_y := 0.0
@@ -39,8 +40,13 @@ func _process(delta):
 	
 	# Atualiza a posição vertical com a velocidade vertical
 	velocity.y = velocity_y
-	
 	# Move e aplica a gravidade se necessário
+	
+	if player_detector_left.is_colliding():
+		spawn_bullet_enemy(-1) # Esquerda
+	if player_detector_right.is_colliding():
+		spawn_bullet_enemy(1) # Direita
+		
 	move_and_slide()
 
 func flip_enemy():
@@ -48,4 +54,13 @@ func flip_enemy():
 	sprite.scale.x *= -1
 	# Se estiver no modo de gravidade 1, não vira o detector de chão
 	if gravity_mode == 1:
-		ground_detector.scale.x *= -1
+		player_detector_left.scale.x *= -1
+		player_detector_right.scale.x *= -1
+		spawn_bullet.position.x *= -1
+		
+		
+func spawn_bullet_enemy(direction):
+	var new_bullet = BULLET_ENEMY_SCENE.instantiate()
+	add_child(new_bullet)
+	new_bullet.global_position = spawn_bullet.global_position
+	new_bullet.direction = direction # Assumindo que a cena da bala tem uma variável 'direction' para controlar sua direção
